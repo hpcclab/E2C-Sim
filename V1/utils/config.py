6a8c9103aaf55026baa7b05fde11a63adab00218
Tony Huynh <tony.huynh@dxc.com>
2022-11-07 00:23:04 -0600

    Updated config.json validations

--- V1/utils/config.py
+++ V1/utils/config.py
@@ -26,6 +26,34 @@ def load_config(path_to_config = './config.json'):
     data = json.loads(data)
     return data
 
+def create_deadline(deadline_value):
+    deadline = 45.0
+    try:
+        if deadline_value < 0:
+            print("WARNING: Value for deadline should not be negative")
+        if deadline_value != 0:
+            deadline = abs(float(deadline_value))
+        else:
+            print("ERROR: Value for deadline cannot be 0. 45.0 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for deadline. 45.0 will be set instead.")
+        deadline = 45.0
+    return deadline
+
+def create_idle_power(value):
+    idle_power = 5
+    try:
+        if value < 0:
+            print("WARNING: Value for idle_power should not be negative")
+        if value != 0:
+            idle_power = abs(int(value))
+        else:
+            print("ERROR: Value for idle_power cannot be 0. 5 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for idle_power. 5 will be set instead.")
+        idle_power = 5
+    return idle_power
+
 def create_task_types(task_types_info):
     task_types = []
     task_type_names = []
@@ -37,7 +65,7 @@ def create_task_types(task_types_info):
             urgency = UrgencyLevel.BESTEFFORT
         elif urgency == 'urgent':
             urgency = UrgencyLevel.URGENT
-        deadline = float(task_type['deadline'])
+        deadline = create_deadline(task_type['deadline'])
         task_types.append(TaskType(id, name, urgency,deadline))
         task_type_names.append(name)
     return task_types, task_type_names
@@ -51,16 +79,47 @@ def create_machine_types(machines_info):
     for id, machine_type in enumerate(machines_info):
         #type_id = machine_type['type_id']        
         name = machine_type['name']
-        power = int(machine_type['power'])
-        idle_power = int(machine_type['idle_power'])
-        replicas = int(machine_type['replicas'])
+        power = create_power(machine_type['power'])
+        idle_power = create_idle_power(machine_type['idle_power'])
+        replicas = create_replicas(machine_type['replicas'])
         type = MachineType(id, name, power,idle_power, replicas)
         no_of_machines += replicas
         machine_types.append(type)
-        machine_type_names.append(name)
-        
-        
+        machine_type_names.append(name)  
     return machine_types, machine_type_names, no_of_machines
+
+def create_power(value):
+    power = 50
+    try:
+        if value < 0:
+            print("WARNING: Value for power should not be negative")
+        if value != 0:
+            power = abs(int(value))
+        else:
+            print("ERROR: Value for power cannot be 0. 50 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for power. 50 will be set instead.")
+        power = 50
+    return power
+
+def create_replicas(value):
+    replicas = 1
+    try:
+        if value < 0:
+            print("WARNING: Value for replicas should not be negative")
+        if value != 0:
+            replicas = abs(int(value))
+        else:
+            print("ERROR: Value for replicas cannot be 0. 1 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for replicas. 1 will be set instead.")
+        replicas = 1
+    return replicas
+
+
+    
+
+
 
 def find_task_type(task_type_name):
     try:
@@ -74,18 +133,130 @@ def find_task_type(task_type_name):
 def set_scheduler(scheduler):
     global scheduling_method
     scheduling_method = scheduler
+    if (str(scheduler) == 'EE' or 'MM' or 'FEE' or 'MSD' or 'MMU' or 'FCFS' or 'MECT' or 'MEET' ):
+        scheduling_method = scheduler
+    else:
+        print('ERROR: Invalid Scheduling Method. FCFS will be set instead.')
+        scheduling_method = 'FCFS'
 
 def get_scheduler():
     global scheduling_method
     return scheduling_method
 
+
 def set_machine_queue_size(size):
     global machine_queue_size
-    machine_queue_size = machine_queue_size
+    machine_queue_size = 3
+    try:
+        if size < 0:
+            print("WARNING: Value for machine_queue_size should not be negative")
+        if size != 0:
+            machine_queue_size = abs(int(size))
+        else:
+            print("ERROR: Value for machine_queue_size cannot be 0. 3 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for machine_queue_size. 3 will be set instead.")
+        machine_queue_size = 3
+
 
 def get_machine_queue_size():
     global machine_queue_size
     return machine_queue_size
+
+def set_capacity(capacity_value):
+    global capacity
+    global total_energy
+    global available_energy
+    capacity = 500.0
+    try:
+        if capacity_value < 0:
+            print("WARNING: Value for capacity should not be negative.")
+        if capacity_value != 0:
+            capacity = abs(int(capacity_value))
+        else:
+            print("ERROR: Value for capacity cannot be 0. 500.0 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for capacity. 500.0 will be set instead.")
+    total_energy = capacity * 3600
+    available_energy = total_energy
+
+def set_batch_queue_size(value):
+    global batch_queue_size
+    batch_queue_size = 3
+    try:
+        if value < 0:
+            print("WARNING: Value for batch_queue_size should not be negative.")
+        if value != 0:
+            batch_queue_size = abs(int(value))
+        else:
+            print("ERROR: Value for batch_queue_size cannot be 0. 3 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for batch_queue_size. 3 will be set instead.")
+
+def set_fairness_factor(value):
+    global fairness_factor
+    fairness_factor = 1.0
+    try:
+        if value < 0:
+            print("WARNING: Value for fairness_factor should not be negative.")
+        if value != 0:
+            fairness_factor = abs(float(value))
+        else:
+            print("ERROR: Value for fairness_factor cannot be 0. 1.0 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for fairness_factor. 1.0 will be set instead.")
+
+def set_bandwidth(value):
+    global bandwidth
+    bandwidth = 15000.0
+    try:
+        if value < 0:
+            print("WARNING: Value for bandwidth should not be negative.")
+        if value != 0:
+            bandwidth = abs(float(value))
+        else:
+            print("ERROR: Value for bandwidth cannot be 0. 15000.0 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for bandwidth. 15000.0 will be set instead.")
+
+def set_network_latency(value):
+    global network_latency
+    network_latency = .015
+    try:
+        if value < 0:
+            print("WARNING: Value for network_latency should not be negative.")
+        if value != 0:
+            network_latency = abs(float(value))
+        else:
+            print("ERROR: Value for network_latency cannot be 0. 0.015 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for network_latency. 0.015 will be set instead.")
+
+def set_gui(value):
+    global gui
+    gui = 1
+    try:
+        if value < 0:
+            print("WARNING: Value for gui should not be negative.")
+        if value != 0:
+            gui = abs(float(value))
+        else:
+            print("ERROR: Value for gui cannot be 0. 1 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for gui. 1 will be set instead.")
+
+def set_verbosity(value):
+    global verbosity
+    verbosity = 3
+    try:
+        if value < 0:
+            print("WARNING: Value for verbosity should not be negative.")
+        if value != 0:
+            verbosity = abs(float(value))
+        else:
+            print("ERROR: Value for verbosity cannot be 0. 3 will be set instead.")
+    except ValueError as val_err:
+        print("ERROR: Invalid value for verbosity. 3 will be set instead.")
 
 def init():
     global event_queue
@@ -94,6 +265,7 @@ def init():
     global task_types, task_type_names
     global cloud
 
+    global capacity
     global settings    
     global scheduling_method
     global fairness_factor
@@ -121,26 +293,21 @@ def init():
     machine_types, machine_type_names, no_of_machines = create_machine_types(data['machines'])
     machines = []
 
-    capacity = float(data['battery'][0]['capacity'])
-    total_energy = capacity * 3600
-    available_energy = total_energy
+    set_capacity(data['battery'][0]['capacity'])
 
-    machine_queue_size = int(data['parameters'][0]['machine_queue_size'])
-    batch_queue_size = int(data['parameters'][0]['batch_queue_size'])     
-    fairness_factor = float(data['parameters'][0]['fairness_factor'])
-    bandwidth = float(data['cloud'][0]['bandwidth'])
-    network_latency = float(data['cloud'][0]['network_latency'])
+    set_machine_queue_size(data['parameters'][0]['machine_queue_size'])
+    # machine_queue_size = int(data['parameters'][0]['machine_queue_size'])
+    set_batch_queue_size(data['parameters'][0]['batch_queue_size'])     
+    set_fairness_factor(data['parameters'][0]['fairness_factor'])
+    set_bandwidth(data['cloud'][0]['bandwidth'])
+    set_network_latency(data['cloud'][0]['network_latency'])
 
-    if (data['parameters'][0]['scheduling_method'] == 'EE' or 'MM' or 'FEE' or 'MSD' or 'MMU' or 'FCFS' or 'MECT' or 'MEET' ):
-        set_scheduler(data['parameters'][0]['scheduling_method'])
-    else:
-        print('Invalid Scheduling Method')
-        set_scheduler('FCFS')
-
+    set_scheduler(data['parameters'][0]['scheduling_method'])
+    
 
     settings = data['settings'][0] 
-    gui = int(settings['gui']) 
-    verbosity = int(settings['verbosity'])
+    set_gui(settings['gui']) 
+    set_verbosity(settings['verbosity'])
     path_to_output = settings['path_to_output']
     path_to_workload = settings['path_to_workload']
 
@@ -148,6 +315,13 @@ def init():
         log = open(f"{path_to_output}/log.txt",'w')
     except OSError as err:
         print(err)
+        path_to_output = "./output"
+
+    try:
+        log = open(f'{path_to_workload}/log.txt', 'w')
+    except OSError as err:
+        print(err)
+        path_to_workload = '.workloads'
 
     
     
