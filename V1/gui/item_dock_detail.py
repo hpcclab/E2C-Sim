3e25ef69631fc0788884edd95ff8eb08c5b6cf00
Ali Mokhtari <ali.mokhtaary@gmail.com>
2023-05-14 19:36:30 -0500

    etc load label fixed

--- V1/gui/item_dock_detail.py
+++ V1/gui/item_dock_detail.py
@@ -268,6 +268,7 @@ class ItemDockDetail(QMainWindow):
                                                     filter='*.csv.eet')
         if path[0]:
             self.path_to_etc = path[0]
+            self.etc_path_entry.setText(self.path_to_etc)
 
         self.write_etc_matrix()
         self.eet_loaded = True
